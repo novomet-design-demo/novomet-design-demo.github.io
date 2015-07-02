dd4e0bdc7c7e9d2e35dfff0bddb1e08f51b7aeac
Anastasia-Epanova <epano@mail.ru>
2015-07-02 13:36:46 +0500

    Окно состояния конкретного объекта

--- js/javascript.js
+++ js/javascript.js
@@ -65,7 +65,7 @@ function setColsSize(){
 }
 
 function tableToBlocks(){
-	$('.table-responsive').html(wellToHtml()+wellToHtml()+wellToHtml()+wellToHtml());
+	$('.page_content').html(wellToHtml()+wellToHtml()+wellToHtml()+wellToHtml());
 	getContentSize();
 }
 /*
@@ -116,7 +116,7 @@ function wellToHtml(){
 	return (
 		'<div class="col-md-4 col-sm-6 col-xs-12 wellBlock">'+
 			'<div class="wellBlockHeader col-md-12 col-sm-12 col-xs-12"> Векторнефть > Призабойненское > Куст 123 > Скважина 319</td></div>'+
-			'<div class="wellBlockContent col-md-12 col-sm-12 col-xs-12">'+
+			'<div class="wellBlockContent col-md-12 col-sm-12 col-xs-12" ondblclick="objectInformation()">'+
 				'<table class="table">'+
 					'<tr><th>Параметр</th><th class="value">Значение</th><th class="tableBorder"></th><th>Параметр</th><th class="value">Значение</th></tr>'+
 					'<tr><td>Режим</td><td class="value">Ручной</td><td class="tableBorder"></td><td>Напряжение Ud, В</td><td class="value">555</td></tr>'+
@@ -140,3 +140,100 @@ function wellToHtml(){
 		'</div>'
 	);
 } 
+
+function objectInformation(){
+	$('.page_content').html('<div class="tabs"><div class="selected-tab">Данные</div><div>Уставки</div><div>Архив</div><div>Информация</div><div>Загр. файлы</div></div><div class="panel col-md-12 row information"></div>');
+	$('.panel').html(informationToHtml());
+	getContentSize();
+}
+
+function informationToHtml(){
+	return(
+		'<div class="col-md-12 row information">'+
+				'<div class="col-md-10">'+
+					'<div class="wellBlockHeader col-md-12 col-sm-12 col-xs-12"> Векторнефть > Призабойненское > Куст 123 > Скважина 319</td></div>'+
+					'<div class="wellBlockContent col-md-12 col-sm-12 col-xs-12">'+
+						'<table class="table">'+
+							'<tr><th>Параметр</th><th class="value">Значение</th><th class="tableBorder"></th><th>Параметр</th><th class="value">Значение</th></tr>'+
+							'<tr><td>Режим</td><td class="value">Ручной</td><td class="tableBorder"></td><td>Напряжение Ud, В</td><td class="value">555</td></tr>'+
+							'<tr><td>Частота, Гц</td><td class="value">60.2</td><td class="tableBorder"></td><td>Вых напряж ПЧ, В</td><td class="value">444</td></tr>'+
+							'<tr><td>Частота зад, Гц</td><td class="value">60.2</td><td class="tableBorder"></td><td>Вых ток ПЧ, А</td><td class="value">124.4 123.2 132.4</td></tr>'+
+							'<tr><td class="warningRow">загрузка, %</td><td class="value warningRow">77.4</td><td class="tableBorder"></td><td class="errorRow">Темпер радиат, С</td><td class="value errorRow">116.5</td></tr>'+
+							'<tr><td class="warningRow">Ток двигателя, А</td><td class="value warningRow">24.4 23.2 32.4</td><td class="tableBorder"></td><td>Вх ток, А</td><td class="value">124.4 123.2 132.4</td></tr>'+
+							'<tr><td class="warningRow">Дисб тока двигателя, %</td><td class="value warningRow">12.0</td><td class="tableBorder"></td><td>Дисб вх тока, %</td><td class="value">12.0</td></tr>'+
+							'<tr><td class="warningRow">Напряж двиг, В</td><td class="value warningRow">3889</td><td class="tableBorder"></td><td>Межфаз напряж, В</td><td class="value">400 410 412</td></tr>'+
+							'<tr><td class="warningRow">Сопр изол, кОм</td><td class="value warningRow">9999</td><td class="tableBorder"></td><td>Вх напряж АВС, В</td><td class="value">220 222 212</td></tr>'+
+							'<tr><td>Активн мощн, кВт</td><td class="value">555.5</td><td class="tableBorder"></td><td>Дисб вх напряж, %</td><td class="value">12.0</td></tr>'+
+							'<tr><td>Полная мощн, кВА</td><td class="value">555.5</td><td class="tableBorder"></td><td>Дав на прием, Мпа</td><td class="value">2.534</td></tr>'+
+							'<tr><td>Коэфф мощности</td><td class="value">0.875</td><td class="tableBorder"></td><td class="errorRow">Темпер двиг, С</td><td class="value errorRow">160.4</td></tr>'+
+						'</table>'+
+					'</div>'+
+					'<div class="wellBlockFooter col-md-12 col-sm-12 col-xs-12">'+
+						'<div class="col-md-4 col-sm-4 col-xs-4 footerPart"><input class="startButton" type="button" value="Пуск"></div>'+
+						'<div class="col-md-4 col-sm-4 col-xs-4 footerPart"><input class="stopButton" type="button" value="Стоп"></div>'+
+						'<div class="col-md-4 col-sm-4 col-xs-4 footerPart"><input class="warningButton" type="button" value="Квитировать"></div>'+
+					'</div>'+
+				'</div>'+
+				'<div class="col-md-2" style="height: 100%;">'+
+					'<div class="rightCol">'+
+						'<input type="button" value="Отобразить все"/>'+
+						'<input type="button" value="Запросить данные"/>'+
+					'</div>'+
+				'</div>'+
+		'</div>'
+	);
+}
+
+function ustToHtml(){
+	return(
+		'<div class="panel col-md-12">'+
+			'<div class="col-md-3">'+
+				'<div class="well">'+
+					'<ul class="nav nav-list">'+
+						'<li class="nav-header">Оборудование</li>'+
+						'<li class="active">Станция</li>'+
+						'<li>Двигатель</li>'+
+						'<li>ТМПН</li>'+
+						'<li>ЧРП</li>'+
+						'<li>Тип ТМС</li>'+
+					'</ul>'+
+					'<ul class="nav nav-list">'+
+						'<li class="nav-header">Защиты</li>'+
+						'<li class="not-selected">'+
+							'<ul class="nav nav-list">'+
+								'<li class="nav-header">Защиты по сети</li>'+
+								'<li>Превышение питания</li>'+
+								'<li>Снижение питания</li>'+
+								'<li>Дисбаланс напряжения</li>'+
+							'</ul>'+
+						'</li>'+
+						'<li class="not-selected">'+
+							'<ul class="nav nav-list">'+
+								'<li class="nav-header">Защиты двигателя</li>'+
+								'<li>Перегруз</li>'+
+								'<li>Недогруз</li>'+
+								'<li>Турбинное вращение</li>'+
+							'</ul>'+
+						'</li>'+
+						'<li class="not-selected">'+
+							'<ul class="nav nav-list">'+
+								'<li class="nav-header">Защиты по ТМС</li>'+
+								'<li>Давление на приеме насоса</li>'+
+								'<li>Перегрев двигателя</li>'+
+								'<li>Сопротивление изоляции</li>'+
+							'</ul>'+
+						'</li>'+
+						'<li class="not-selected">'+
+							'<ul class="nav nav-list">'+
+								'<li class="nav-header">Защиты дополнительные</li>'+
+								'<li>Защита ЧРП</li>'+
+							'</ul>'+
+						'</li>'+
+					'</ul>'+
+				'</div>'+
+			'</div>'+
+			'<div class="col-md-9">'+
+			'</div>'+
+		'</div>'
+	);
+}
